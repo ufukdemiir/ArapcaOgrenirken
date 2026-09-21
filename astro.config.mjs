655f9df7153bc6b39ef21b0c56e9e789977bebd6
Ufuk Demir <174389657+ufukdemiir@users.noreply.github.com>
2026-09-21 15:20:23 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ufukdemir.github.io',
+  site: 'https://ufukdemir.github.io/ArapcaOgrenirken',
   base: '/ArapcaOgrenirken',
   integrations: [tailwind()],
   outDir: './dist',
